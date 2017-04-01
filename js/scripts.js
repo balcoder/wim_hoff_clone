9a9c1df3c51164103fd1a3ce9c100f4ee80dae1d
Des Barrett <des.barrett@gmail.com>
2017-04-01 10:04:47 +0100

    added fontawesome

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-$('ul.nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-}, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+$(document).ready(function(){ //dropdowns have to be in document.ready to fire
+  $('ul.nav li.dropdown').hover(function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+  }, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+  });
 });
